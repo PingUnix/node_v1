34f6f14b21e0529acd95badf8c355d7c33547f35
SandyLinux <mailliuping@gmail.com>
2018-03-09 14:12:17 -0500

    create json table classroom

--- db_json_v2.js
+++ db_json_v2.js
@@ -2,11 +2,25 @@ var db = require('mysql');
 //REST model 
 // include file system module to read json file
 var fs = require('fs');
-
 var con = db.createConnection({
     host:'localhost',
     user:'root',
     password:'123',
     database:'mydb'}
 );
- 
+
+var sql = 'create table classroom (room_id int not null auto_increment, label varchar(255), tags json default null, primary key (room_id))';
+
+con.connect(function(err,result){
+    if(err) throw err;
+        console.log(result);
+    
+    con.query(sql, function(err, result){
+        if (err ) 
+            throw err;
+        console.log(result);
+
+    })
+
+});
+
